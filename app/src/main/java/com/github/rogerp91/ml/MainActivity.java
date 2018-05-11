436663c8c5cd2bc86ab6c4b0772522a6a4c0951c
Roger Patiño <roger@darwoft.com>
2018-05-11 17:35:05 -0300

    feat: Inicializar

---
+++ app/src/main/java/com/github/rogerp91/ml/MainActivity.java
@@ -0,0 +1,13 @@
+package com.github.rogerp91.ml;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
